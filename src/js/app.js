35e212b17d626f83f1cc19a5870c3345b908bbee
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2021-11-03 16:44:55 +0300

    homework2

--- src/js/app.js
+++ src/js/app.js
@@ -5,15 +5,28 @@ console.log('worked');
 
 console.log(sum([1, 2]));
 
+// Status of health
 export const indicate = function (player) {
-    if (player.health > 50) {
-        return 'healthy'
-    }
-    if (player.health > 14) {
-        return 'wounded'
-    }
-    else {
-        return 'critical'
-    }
-}
+  if (player.health > 50) {
+    return 'healthy';
+  }
+  if (player.health > 14) {
+    return 'wounded';
+  }
 
+  return 'critical';
+};
+
+// Sorting
+
+export const sorting = function (players) {
+  return players.sort((a, b) => {
+    if (a.health > b.health) {
+      return -1;
+    }
+    if (a.health < b.health) {
+      return 1;
+    }
+    return 0;
+  });
+};
