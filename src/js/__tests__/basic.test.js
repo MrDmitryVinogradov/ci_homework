cadcce7c4812b3023af7050756d8c39664033b30
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2021-11-03 14:24:14 +0300

    Homework

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,17 @@
 import sum from '../basic';
-
+import { indicate } from '../app';
 test('should sum', () => {
   const result = sum([1, 2, 3]);
 
   expect(result).toBe(6);
+});
+describe.each([
+  ['Маг', 90, 'healthy'],
+  ['Маг', 30, 'wounded'],
+  ['Маг', 10, 'critical']
+])(
+('should indicate status for %i health'), 
+(name, health, expected) => {
+  const result = indicate({name, health});
+  expect(result).toBe(expected);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,16 @@ import sum from './basic';
 console.log('worked');
 
 console.log(sum([1, 2]));
+
+export const indicate = function (player) {
+    if (player.health > 50) {
+        return 'healthy'
+    }
+    if (player.health > 14) {
+        return 'wounded'
+    }
+    else {
+        return 'critical'
+    }
+}
+
